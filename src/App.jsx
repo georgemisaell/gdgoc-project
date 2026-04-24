ce9cc40f3194df5c7ce79293b52b2f1712f9a1e0
georgemisaell <georgemisaelgantume@gmail.com>
2026-04-24 20:28:08 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-container">
+      <div className="blob"></div>
+      <div className="blob second"></div>
+
+      <div className="glass-card">
+        <h1 className="title">
+          GDGOC <span className="highlight">2026</span>
+        </h1>
+        <p className="subtitle">Start Your ReactJS Journey!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
